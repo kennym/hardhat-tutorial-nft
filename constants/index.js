1b0b78ecfbe2243443b4afce7d5a5eefc33ea0ed
Kenny Meyer <kenny@kennymeyer.net>
2022-09-09 22:35:39 -0400

    initial commit

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x28f7b734dbd42afddac1ab9da83819010b90ede1";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
